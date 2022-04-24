57f6f1688a1f4f142c5c2d7609ee691699cd586a
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-04-24 13:45:00 -0500

    deleted touch controls

--- script.js
+++ script.js
@@ -44,21 +44,6 @@ function jump() {
     }, 5   );
 }                    
 
-function touchHandler(e) {
-    jumping = 1;
-    let jumpCount = 0;
-    let jumpInterval = setInterval(function () {
-        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if ((characterTop > 6) && (jumpCount < 15)) {
-            character.style.top = (characterTop - 5) + "px";
-        }
-        if (jumpCount > 20) {
-            clearInterval(jumpInterval);
-            jumping = 0;
-            jumpCount = 0;
-        }
-        jumpCount++;
-    }, 5   );
-}                     
+                 
 
 
