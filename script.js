4a87d697617409b33f0ba2ecb36fb8edd57c587c
jaguirre815 <97426370+jaguirre815@users.noreply.github.com>
2022-04-24 11:21:21 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+//variables
+let block = document.getElementById("block");
+let hole = document.getElementById("hole");
+let character = document.getElementById("character");
+let jumping = 0;
+let counter = 0;
+
+//pipes animaiton
+hole.addEventListener('animationiteration', () => {
+    let random = -((Math.random() * 300) + 150);
+    hole.style.top = random + "px";
+    counter++;
+});
+//
+setInterval(function () {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if (jumping == 0) {
+        character.style.top = (characterTop + 3) + "px";
+    }
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    let cTop = -(500 - characterTop);
+    if ((characterTop > 480) || ((blockLeft < 50) && (blockLeft > -20) && ((cTop < holeTop) || (cTop > holeTop + 120)))) {
+        alert("You Die Homie! Score: " + (counter - 1));
+        character.style.top = 100 + "px";
+        counter = 0;
+    }
+}, 10  );
+
+function jump() {
+    jumping = 1;
+    let jumpCount = 0;
+    let jumpInterval = setInterval(function () {
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if ((characterTop > 6) && (jumpCount < 15)) {
+            character.style.top = (characterTop - 5) + "px";
+        }
+        if (jumpCount > 20) {
+            clearInterval(jumpInterval);
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    }, 5   );
+}                    
+
+
+//testing  dd
