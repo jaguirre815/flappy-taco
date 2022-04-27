6aa87826b69f3d8e067bca1dfa21805e200605e2
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-04-26 19:04:57 -0500

    bottom fixed

--- script.js
+++ script.js
@@ -7,22 +7,26 @@ let counter = 0;
 
 //pipes animaiton
 hole.addEventListener('animationiteration', () => {
-    let random = -((Math.random() * 400) + 150);
+    let random = -((Math.random() * 500) + 100);
     hole.style.top = random + "px";
     counter++;
 });
-//
+//gravity
 setInterval(function () {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if (jumping == 0) {
         character.style.top = (characterTop + 3) + "px";
     }
+
+
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    let cTop = -(500 - characterTop);
-    if ((characterTop > 460) || ((blockLeft < 50) && (blockLeft > -20) && ((cTop < holeTop) || (cTop > holeTop + 120)))) {
-        alert("You Are Dead Homie! Score: " + (counter - 1));
-        character.style.top = 100 + "px";
+    let cTop = -(450 - characterTop);
+
+
+    if ((characterTop > 400) || ((blockLeft < 5) && (blockLeft > -5) && ((cTop < holeTop) || (cTop > holeTop + 500)))) {
+        //alert("You Are Dead Homie! Score: " + (counter - 1));
+        character.style.top = 20 + "px";
         counter = 0;
     }
 }, 10  );
